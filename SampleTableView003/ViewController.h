77ce5504bfb0bc4f5a5ac51ca701f4ce5436abd1
ei17ringo <ei17ringo@gmail.com>
2014-10-21 12:39:31 +0800

    Initial Commit

---
+++ SampleTableView003/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleTableView003
+//
+//  Created by Eriko Ichinohe on 2014/10/21.
+//  Copyright (c) 2014年 Eriko Ichinohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleTableView003/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SampleTableView003
+//
+//  Created by Eriko Ichinohe on 2014/10/21.
+//  Copyright (c) 2014年 Eriko Ichinohe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
