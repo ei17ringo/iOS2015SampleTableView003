8b6cca7813a21bf33baedec23be5ff1302ad8899
ei17ringo <ei17ringo@gmail.com>
2015-01-17 13:01:52 +0800

    プログラミング言語辞典

--- SampleTableView003/ViewController.m
+++ SampleTableView003/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "DetailViewController.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,48 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _programmingArray = @[@"C",@"Java",@"Objective-C",@"C++",@"HTML",@"CSS",@"Javascript",@"MySQL",@"Ruby"];
+    
+    // NSLog(@"配列の中身の数:%d",_coffeeArray.count);
+    
+    _proTableView.delegate = self;
+    _proTableView.dataSource = self;
+}
+
+//行数を返す
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _programmingArray.count;
+}
+
+//セルに文字を表示する
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    //定数を宣言（static = 静的)
+    static NSString *CellIdentifer = @"Cell";
+    
+    //セルの再利用
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifer];
+    
+    if(cell == nil){
+        //セルの初期化とスタイルの決定
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifer];
+    }
+    
+    //cell.textLabel.text = [NSString stringWithFormat:@"行番号=%d",indexPath.row];
+    
+    cell.textLabel.text = _programmingArray[indexPath.row];
+    
+    return cell;
+}
+
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    DetailViewController *dvc = [segue destinationViewController];
+    dvc.selectNum = self.proTableView.indexPathForSelectedRow.row;
+    
 }
 
 - (void)didReceiveMemoryWarning {
